23ff3a7afe08b9e10d91c173644ad510a1435b47
Snehaa <94774019+sneehaa@users.noreply.github.com>
2024-06-23 08:53:36 +0545

    database & env files created

--- index.js
+++ index.js
@@ -1,14 +1,56 @@
+// importing
 const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./database/db');
+const cors = require('cors');
+const cloudinary = require('cloudinary');
+const acceptMultimedia = require('connect-multiparty');
+const { authGuard } = require('./middleware/authGuard');
 
+// Making express app
 const app = express();
 
-const PORT = 5500;
-app.listen(5500, () => {
-    console.log(`Server running on port ${PORT}`)
+// dotenv config
+dotenv.config();
+
+// cloudinary config
+
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+app.use(acceptMultimedia())
+
+// cors config to accept request from frontend
+const corsOptions = {
+    origin: true,
+    credentials: true,
+    optionSuccessStatus: 200
+};
+app.use(cors(corsOptions))
+
+// mongodb connection
+connectDB();
+
+// Accepting json data
+app.use(express.json());
+
+// creating test route
+app.get("/test", (req,res) => {
+    res.status(200).send("Hello");
 })
 
-app.get('/', (req, res) => {
 
-    res.send('Hello World!');
+// defining port
+const PORT = process.env.PORT;
+// run the server
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`)
+    console.log("Connected to Database")
+})
 
-});
+// exporting app
+module.exports = app;
