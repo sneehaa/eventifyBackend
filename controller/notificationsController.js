6e8877a562055ab6e6c8e0cc8a55c43d2609ed8f
Snehaa <94774019+sneehaa@users.noreply.github.com>
2024-07-15 11:29:28 +0545

    Created notificationsController.js

---
+++ controller/notificationsController.js
@@ -0,0 +1,27 @@
+
+const Notification = require('../model/notificationModel');
+
+const addNotification = async (req, res) => {
+  try {
+    const notification = new Notification(req.body);
+    await notification.save();
+    res.status(201).json(notification);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const getNotifications = async (req, res) => {
+  try {
+    const notifications = await Notification.find();
+    res.status(200).json(notifications);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+
+module.exports = {
+ addNotification,
+  getNotifications
+}
