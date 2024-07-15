985f3315e39e091fc01052517b52099cb89f0c19
Snehaa <94774019+sneehaa@users.noreply.github.com>
2024-07-15 11:38:12 +0545

    created checkoutModel.js

---
+++ model/checkoutModel.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const checkoutSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  ticketDetails: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      count: {
+        type: Number,
+        required: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Checkout = mongoose.model('Checkout', checkoutSchema);
+
+module.exports = Checkout;
